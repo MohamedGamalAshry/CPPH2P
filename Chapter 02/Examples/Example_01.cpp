d4b914966ed8dfdf29440d3a0ec33fce077e64d0
Mohamed Ashry <MohamedAs@dspace.de>
2020-11-11 15:52:53 +0100

    write the first Example_01.cpp in Chapter 02

---
+++ Chapter 02/Examples/Example_01.cpp
@@ -0,0 +1,14 @@
+// Name: Example_01.cpp
+// Description:First example in Chapter 02, C++ How to Program
+
+// enable the program to output data on the screen by including the Preprocessing Directive
+#include<iostream>		
+
+// Function main begins the program excution:
+
+int main(){
+	std::cout << "Welcome to C++ !! \n" ;	// display text message
+	
+	return 0;		// indicate that program ended successfully by returning 0
+	
+}	//	end of main function
